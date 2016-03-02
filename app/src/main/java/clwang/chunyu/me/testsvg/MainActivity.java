8c9976aeb7e83c080934a5e3b9c1c326f0593c6b
spike <morndragon@126.com>
2016-03-02 09:27:00 +0800

    修改

--- app/src/main/java/clwang/chunyu/me/testsvg/MainActivity.java
+++ app/src/main/java/clwang/chunyu/me/testsvg/MainActivity.java
@@ -3,6 +3,8 @@ package clwang.chunyu.me.testsvg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pixplicity.sharp.Sharp;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
